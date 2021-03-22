2b98a05a0eb4792e90f74a4d8cdbb0883d8b3202
Tristen <janzentristen@yahoo.com>
2021-03-22 13:18:31 -0500

    Added files from call.

--- src/com/pyramid/academy/projects/Board.java
+++ src/com/pyramid/academy/projects/Board.java
@@ -1,5 +1,24 @@
 package com.pyramid.academy.projects;
 
 public class Board {
+        simulateTurn(Player player){
+            boolean move = false;
+            do{
+                Piece p;
+                int y;
+                int x;
+                do {
+                    String str = player.getInputPiece();
+                    x = player.strToLocationX(str);
+                    y = player.strToLocationY(str);
+                    p = getPiece(x, y);
+                }while(p.getColor()!="empty");
+                String str = player.getInputLocation();
+                x = player.strToLocationX(str);
+                y = player.strToLocationY(str);
+                move = canMove(p,x,y);
+            }while(!move);
+            checkForKing();
 
+        }
 }
--- src/com/pyramid/academy/projects/Main.java
+++ src/com/pyramid/academy/projects/Main.java
@@ -3,6 +3,16 @@ package com.pyramid.academy.projects;
 public class Main {
 
     public static void main(String[] args) {
-
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Board board = new Board(player1,player2);
+        boolean playerOneTurn = true;
+        do{
+            if(playerOneTurn)
+                board.simulateTurn(board.getPlayer1);
+            else()
+                board.simulateTurn(board.getPlayer2);
+            System.out.print(board);
+        }while(!Board.checkForWin());
     }
 }
