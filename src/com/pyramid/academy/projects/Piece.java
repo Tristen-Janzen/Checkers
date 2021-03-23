5d90ad95937d9a6cffb68ab44782424da682ebd9
Juan Trejo <jtrejo4545@gmail.com>
2021-03-23 09:34:28 -0500

    Piece getters and setters

--- src/com/pyramid/academy/projects/Piece.java
+++ src/com/pyramid/academy/projects/Piece.java
@@ -1,5 +1,18 @@
 package com.pyramid.academy.projects;
 
 public class Piece {
-
+    private String color;
+    private boolean king;
+    public String getColor(){
+        return this.color;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    public boolean getKing(){
+        return this.king;
+    }
+    public void setKing(boolean king){
+        this.king = king;
+    }
 }
